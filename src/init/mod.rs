e576f22a3176e1b279d7cd28640ecbcbe17f93ce
Ryooooooga <eial5q265e5@gmail.com>
2021-09-08 00:05:56 +0900

    Fix init

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -3,12 +3,12 @@ use shell_escape::escape;
 use std::borrow::Cow;
 
 static INIT_SCRIPT: &str = include_str!("nekko.zsh");
-static EXPAND_FUNCTION: &str = "__nekko::expand";
+static SEARCH_FUNCTION: &str = "__nekko::search";
 
 pub fn run(args: &InitArgs) {
     print!("{}", INIT_SCRIPT);
 
     if let Some(bindkey) = &args.bindkey {
-        println!("bindkey {} {}", escape(Cow::from(bindkey)), EXPAND_FUNCTION);
+        println!("bindkey {} {}", escape(Cow::from(bindkey)), SEARCH_FUNCTION);
     }
 }
